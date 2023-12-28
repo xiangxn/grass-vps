8558ea2bb6d164e879c0b19391b5155ce687110c
necklace <xiangxn@163.com>
2023-12-28 23:56:29 +0800

    add port param

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+PORT=1087
+if [ ! -n "$1" ];then
+    PORT=1087
+else
+    PORT=$1
+fi
 curl -s http://myip.ipip.net
-export GLOBAL_AGENT_HTTP_PROXY=http://172.17.0.1:1087
+export GLOBAL_AGENT_HTTP_PROXY=http://172.17.0.1:${PORT}
 /root/.nvm/versions/node/v19.9.0/bin/node -r 'global-agent/bootstrap' --max-http-header-size=1073741824 ./src/app.js
